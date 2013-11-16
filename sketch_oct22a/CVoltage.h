a0043c865eceaad8d569af79cdf2b4cd85cd2675
Alienwaren <dkpolan3@gmail.com>
2013-11-16 13:08:13 +0100

    Cofnięcie zmian z powodu błędów

--- sketch_oct22a/CVoltage.h
+++ sketch_oct22a/CVoltage.h
@@ -1,17 +1,15 @@
 #ifndef _H_CVOLTAGE_
 #define _H_CVOLTAGE_
 #include "Arduino.h"
-typedef float Voltage;
 ///
-///@class CVoltage zapewnia opakopanie napięć i ich sptawdzanie
+///@brief Template pozwoli na łatwiejsze tworzenie obiektów reprezentujących napięcia
 ///
+template <typename voltageType>
 class CVoltage
 {
 public:
 	CVoltage(void);
 	~CVoltage(void);
-        protected:
-	Voltage actualSegmentVoltage; ///@var wartość napięcia
-  
-};
+	voltageType voltageAmount;
+}
 #endif ///_H_CVOLTAGE_
